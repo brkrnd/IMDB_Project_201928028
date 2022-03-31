65f0a4cf62c4952ba5df3b7d8282980767acf573
Berk Rende <rendeberk@gmail.com>
2022-03-31 20:48:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/imdb_project_201928028/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.imdb_project_201928028;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
